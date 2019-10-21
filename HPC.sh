2ade26f382c4abcdf793b27a88f2958fc6bab938
Domingos Ferreira Viana Neto <dfvneto@gmail.com>
2019-10-20 21:38:06 -0300

    added gpu information

--- HPC.sh
+++ HPC.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
 # -- INFORMACOES DA CPU -- 
 echo -e "CPU:\n" > info.txt
-echo `cat /proc/cpuinfo | grep -i 'cpu MHz'` >> info.txt
-echo `cat /proc/cpuinfo | grep -i 'vendor_id' | uniq ` >> info.txt
-echo `cat /proc/cpuinfo | grep -i 'cpu cores' | uniq ` >> info.txt
+echo `lscpu | grep -i 'cpu MHz:'` >> info.txt
+echo `lscpu | grep -i 'cpu max MHz'` >> info.txt
+echo `lscpu | grep -i 'cpu min MHz'` >> info.txt
+echo `lscpu | grep -i 'vendor_id' ` >> info.txt
+echo `lscpu | grep -i 'per socket' ` >> info.txt
+echo `lscpu | grep -i 'socket(s)' ` >> info.txt
+echo `lscpu | grep -i 'Model Name' ` >> info.txt
 echo `cat /proc/cpuinfo | grep -i 'cache size' | uniq ` >> info.txt
+dialog --title 'Informações do Sistema' --textbox info.txt 0 0
 
 # -- INFORMACOES DA RAM -- 
-echo -e "\n RAM: \n" >> info.txt
-echo `sudo dmidecode --type 17 | grep -i 'Speed' | uniq ` >> info.txt
+echo -e "RAM: \n" > info.txt
+echo `sudo dmidecode --type 17  | grep  -P '(?<=Speed).*(?=MHz)'` >> info.txt
 echo `cat /proc/meminfo | grep -i 'MemTotal' ` >> info.txt
 echo `cat /proc/meminfo | grep -i 'MemFree' ` >> info.txt
 echo `cat /proc/meminfo | grep -i 'MemAvailable' ` >> info.txt
-
-# -- INFORMACOES DO HD -- 
-echo -e "\n HD: \n" >> info.txt
-echo ` fdisk -l | grep 'Disk /dev/sda'` >> info.txt
-echo `sfdisk -l -uM` >> info.txt # particoes e tamanho 
-
-# -- INFORMACOES DO BARRAMENTO -- 
-echo `lspci` >> info.txt # informação dos barramentos pci e todos os dispositivos plugados a ele.
-echo `lsusb` >> info.txt # informação dos barramentos usb e todos os dispositivos plugados a ele.
-echo `lsmod` >> info.txt # informação dos barramentos modulo e todos os dispositivos plugados a ele.
- 
-
 dialog --title 'Informações do Sistema' --textbox info.txt 0 0
+rm info.txt
+# -- INFORMACOES DO HD -- 
+echo -e "\n HD: \n" > info.txt
+sudo fdisk -l -uM >> info.txt # particoes e tamanho 
+dialog --title 'Informações do Sistema' --textbox info.txt 0 0
+# -- INFORMACOES DO BARRAMENTO -- 
+echo -e "Barramento PCI: \n" > info.txt
+lspci >> info.txt # informação dos barramentos pci e todos os dispositivos plugados a ele.
+dialog --title 'Informações do Sistema' --textbox info.txt 0 0
+echo -e "Barramento USB: \n" > info.txt
+lsusb >> info.txt # informação dos barramentos usb e todos os dispositivos plugados a ele.
+dialog --title 'Informações do Sistema' --textbox info.txt 0 0
+echo -e "Barramento Modular: \n" > info.txt
+lsmod >> info.txt # informação dos barramentos modulo e todos os dispositivos plugados a ele.
+dialog --title 'Informações do Sistema' --textbox info.txt 0 0
+rm info.txt
+# -- INFORMACOES DA GPU --
+echo -e "GPU: \n" > info.txt
+nvidia-smi >> info.txt
 
+
+clear
