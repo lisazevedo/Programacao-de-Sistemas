4a20e6de59b147ec9842fcb32c0d04146fec2b96
Domingos Ferreira Viana Neto <dfvneto@gmail.com>
2019-11-28 19:44:21 -0300

    test

--- HPC.sh
+++ HPC.sh
@@ -83,34 +83,35 @@ while(:;) do
 	2)
 	#Inicio Firewall
 	 	system_ip=$(dialog --inputbox --stdout 'Digite o IP do servidor:' 0 0 )
-		 iptables -t nat -A POSTROUTING -s $system_ip/24 -j MASQUERADE
-		 echo "1" >/proc/sys/net/ipv4/ip_forward
-		 dialog --title 'AVISO' --infobox 'O sistema está habilitado como servidor' 0 0
-		 sleep 5
+		sudo iptables -t nat -A POSTROUTING -s $system_ip/24 -j MASQUERADE
+		echo "1" >/proc/sys/net/ipv4/ip_forward
+		dialog --title 'AVISO' --infobox 'O sistema está habilitado como servidor' 0 0
+		sleep 5
 		while(:;) do
-		REGRA=$(dialog --stdout --nocancel --title 'Menu' --menu 'O que deseja Fazer' 0 0 0 1 "Ver Regras Habilitadas" 2 "Remover todas as Regras" 3 "Bloquear IP" 4 "Roteamento" 0 "Sair")
+		REGRA=$(dialog --stdout --nocancel --title 'Menu' --menu 'O que deseja Fazer' 0 0 0 1 "Ver Regras Habilitadas" 2 "Remover todas as Regras INCLUINDO NAT" 3 "Bloquear IP" 4 "Roteamento" 0 "Sair")
 		case "$REGRA" in
 		0)
 			break;;
 		1)
 			echo -e "Regras de Firewall implementadas:" > info.txt
 			echo -e "-------------------------------------------------------------------------" >> info.txt
-			iptables -L >> info.txt
+			sudo iptables -L >> info.txt
 			echo -e "-------------------------------------------------------------------------" >> info.txt
 			echo -e "Regras implementadas na tabela NAT:" >> info.txt
 			echo -e "-------------------------------------------------------------------------" >> info.txt
-			iptables -L -n -t  nat >> info.txt
+			sudo iptables -L -n -t  nat >> info.txt
 			echo -e "-------------------------------------------------------------------------" >> info.txt
 
 			dialog --title 'Firewall' --textbox info.txt 0 0
 			rm info.txt
 			;;
 		2)
-			iptables -F
+			sudo iptables -F
+			sudo iptables -L -n -t  nat
 			;;
 		3)
 			blocked_ip=$(dialog --inputbox --stdout 'Digite o IP a ser bloqueado:' 0 0 )
-			iptables -I INPUT -s $blocked_ip -j DROP
+			sudo iptables -I INPUT -s $blocked_ip -j DROP
 			;;
 		4)	
 			ROTEAMENTO=$( dialog --stdout --radiolist 'Aperte barra de espaco para selecionar:' 0 0 0 1 'PREROUTING' off 2 'POSTROUTING' off 3 'FORWARD' on )
@@ -121,32 +122,25 @@ while(:;) do
 				ls /sys/class/net > info.txt
 				dialog --title 'Interfaces disponiveis para seleção' --textbox info.txt 0 0
 				select_interface=$(dialog --inputbox --stdout 'Digite a interface de rede a ser utilizada:' 0 0 )
-				iptables -t nat -A PREROUTING -s $source_ip -i $select_interface -j DNAT --to $destiny_ip
+				sudo iptables -t nat -A PREROUTING -s $source_ip -i $select_interface -j DNAT --to $destiny_ip
 				;;
 			2)	
 
 				source_ip=$(dialog --inputbox --stdout 'Digite o IP fonte:' 0 0 )
-                                modified_ip=$(dialog --inputbox --stdout 'Digite o IP modificado:' 0 0 )
-                                ls /sys/class/net > info.txt
-                                dialog --title 'Interfaces disponiveis para seleção' --textbox info.txt 0 0
-                                select_interface=$(dialog --inputbox --stdout 'Digite a interface de rede a ser utilizada:' 0 0 )
-                                iptables -t nat -A POSTROUTING -s $source_ip -o $select_interface -j SNAT --to $modified_ip
-                                ;;
+				modified_ip=$(dialog --inputbox --stdout 'Digite o IP modificado:' 0 0 )
+                ls /sys/class/net > info.txt
+				dialog --title 'Interfaces disponiveis para seleção' --textbox info.txt 0 0
+                select_interface=$(dialog --inputbox --stdout 'Digite a interface de rede a ser utilizada:' 0 0 )
+                sudo iptables -t nat -A POSTROUTING -s $source_ip -o $select_interface -j SNAT --to $modified_ip
+                ;;
 				
 			3)
 				dialog --title 'AVISO' --infobox 'Essa regra irá permitir a entrada de pacotes de protocolo tcp na porta e ip desejados, cuidado pois permitir pacotes pode colocar o seu sistema em risco' 0 0
 				source_ip=$(dialog --inputbox --stdout 'Digite o IP Desejado:' 0 0 )
 				port=$(dialog --inputbox --stdout 'Digite a porta desejada' 0 0)
-
-
-				iptables -A FORWARD -p tcp -d $source_ip --dport $port -j ACCEPT
-
+				sudo iptables -A FORWARD -p tcp -d $source_ip --dport $port -j ACCEPT
 				;;
-
-
 			esac
-
-	
 			;;
 		esac
 		done
@@ -155,23 +149,27 @@ while(:;) do
 	3)
 	#Inicio Proxy
 		while(:;) do
+		ip route | grep default > info.txt
+		dialog --title 'Gateway padrao' --textbox info.txt 0 0
+		gate=$(dialog --inputbox --stdout 'Digite o seu gateway:' 0 0 )
+		dialog --title 'AVISO' --infobox 'A porta 80 sera redirecionada para a porta 3128 para uso do proxy transparente' 0 0
+		sudo iptables -t nat -A PREROUTING -s $gate -p tcp --dport 80 -j REDIRECT --to-port 3128
+		sleep 3
+		sudo service squid start
 		SELECAO=$(dialog --stdout --nocancel --title 'Menu' --menu 'Escolha a informacao desejada' 15 40 9 1 "Bloquear Site"  2 "Limpar Sites Bloqueados")
 		case "$SELECAO" in
 		1)
 			block_site=$(dialog --inputbox --stdout 'Digite o Site que deseja bloquear no modelo > .blockedsite.com:' 0 0 )
-		#	block_site >> inserir aqui arquivo dos sites bloqueados  FUNÇÃO QUE DETERMINA OS SITES BLOQUEADOS
+			echo $block_site >> blocked_sites
 			dialog 'AVISO' --infobox 'Espere alguns minutos para a aplicaçao das regras' 0 0
 			sleep 3
 			sudo systemctl restart squid #restarta o squid para aplicação das regras
 			;;
 		2)
 		#       "" >> inserir aqui arquivo dos sites bloqueados  FUNÇÃO QUE DETERMINA OS SITES BLOQUEADOS
-                	dialog --title 'AVISO' --infobox 'Espere alguns minutos para a aplicaçao das regras' 0 0
+			dialog --title 'AVISO' --infobox 'Espere alguns minutos para a aplicaçao das regras' 0 0
 			sleep 3       
-	       		sudo systemctl restart squid #restarta o squid para aplicação das regras
-			
-
-
+			sudo systemctl restart squid #restarta o squid para aplicação das regras
 			;;
 		esac
 
